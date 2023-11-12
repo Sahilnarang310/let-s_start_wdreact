5bd374281d1e38a65fccefad3808d34f2edb2b2c
Sahilnarang310 <softwaredev310@gamil.com>
2023-11-12 19:19:17 +0530

    Passing Data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
-import './App.css';
+// import './App.css';
 // function App() {
 //   return (
 //     <div className="App">
@@ -26,15 +26,64 @@ import './App.css';
 
 // export default App;
 
-import ExpenseItem  from './components/ExpenseItem';
-function App (){
-
-return (
-
-<div>
-<h2>Let's get started !</h2>
-<ExpenseItem></ExpenseItem>
-</div>
+import ExpenseItem from "./components/ExpenseItem";
+function App() {
+  const expense = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      location: "Mumbai",
+    },
+    { id: "e2", 
+    title: "Meeting With Kangna Ranaut",
+     amount: 79999.49, 
+     date: new Date(2021, 2, 12),
+    location:"Andheri", },
+    {
+      id: "e3",
+      title: "MahaRaja + French Fries",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      location: "Cnaught Place Delhi",
+    },
+    {
+      id: "e4",
+      title: "Extra Large Bucket",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      location: "KFC",
+    },
+  ];
+  return (
+    <div>
+      <h2>Let's get started !</h2>
+      <ExpenseItem
+        title={expense[0].title}
+        amount={expense[0].amount}
+        date={expense[0].date}
+        locationOfExpenditure={expense[0].location}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[1].title}
+        amount={expense[1].amount}
+        date={expense[1].date}
+        locationOfExpenditure={expense[1].location}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[2].title}
+        amount={expense[2].amount}
+        date={expense[2].date}
+        locationOfExpenditure={expense[2].location}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expense[3].title}
+        amount={expense[3].amount}
+        date={expense[3].date}
+        locationOfExpenditure={expense[3].location}
+      ></ExpenseItem>
+    </div>
   );
 }
 export default App;
