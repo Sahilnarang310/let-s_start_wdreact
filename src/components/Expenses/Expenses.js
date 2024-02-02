cadf259efbd4e9ee4aa4f3211be903da89ce38a6
Sahilnarang310 <softwaredev310@gamil.com>
2024-02-02 14:44:13 +0530

    Filter working and add expenses button are functioning properly

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,83 +1,48 @@
-// import React from 'react'
-// import ExpenseItem from './ExpenseItem'
-// import './Expenses.css'
-// import Card from '../UI/Card'
-
-// function Expenses(props) {
-//   return (
-//     <Card classsname='expenses'>
-
-//         {props.expenses && props.expenses.map((expense) => (
-//       <ExpenseItem
-//       title={expense.title}
-//           amount={expense.amount}
-//           date={expense.date}
-//           location={expense.location}
-
-//       />
-//       ))}
-//     </Card>
-//   )
-// }
-
-// export default Expenses
-
 import React, { useState } from "react";
-
-import ExpenseItem from "./ExpenseItem";
-import Card from "../UI/Card";
-import ExpensesFilter from "./ExpensesFilter";
-import "./Expenses.css";
-
+ import ExpenseItem from "./ExpenseItem";
+ import Card from "../UI/Card";
+ import ExpensesFilter from "./ExpensesFilter";
+ import "./Expenses.css";
+import './ExpensesList.css';
+import ExpensesList from './ExpensesList';
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-  };
-  const filteredExpenses = props.items.filter(expense => {
+   };
+
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+let expensesContnet =  <p>No expenses found</p>;
+if(filteredExpenses.length>0){
+  expensesContnet= filteredExpenses.map((expense) => (
+    <ExpenseItem
+      key={expense.id}
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}
+    />
+  ))
+
+}
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
+          
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-          key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+       {expensesContnet}
+          
 
-        {/* <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        /> */}
+       
       </Card>
     </div>
   );
 };
 
-export default Expenses;
+ export default Expenses;
