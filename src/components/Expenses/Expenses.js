eaa833964d2a5348d4a77e6b448b1eff3647169d
Sahilnarang310 <softwaredev310@gamil.com>
2024-02-03 14:25:52 +0530

    ChartBar added successfully!

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,48 +1,34 @@
-import React, { useState } from "react";
- import ExpenseItem from "./ExpenseItem";
- import Card from "../UI/Card";
- import ExpensesFilter from "./ExpensesFilter";
- import "./Expenses.css";
-import './ExpensesList.css';
+import React, { useState } from 'react';
+
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+import './Expenses.css';
+
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-   };
+  };
 
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-let expensesContnet =  <p>No expenses found</p>;
-if(filteredExpenses.length>0){
-  expensesContnet= filteredExpenses.map((expense) => (
-    <ExpenseItem
-      key={expense.id}
-      title={expense.title}
-      amount={expense.amount}
-      date={expense.date}
-    />
-  ))
-
-}
   return (
     <div>
-      <Card className="expenses">
+      <Card className='expenses'>
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
-          
         />
-       {expensesContnet}
-          
-
-       
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
 };
 
- export default Expenses;
+export default Expenses;
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -44,7 +44,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
@@ -94,3 +94,4 @@ const ExpenseForm = (props) => {
 };
 
 export default ExpenseForm;
+
